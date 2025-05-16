901658cc43a7fe8563af076c063d373c0bc3e797
showgayaki <knk.lee.kj@gmail.com>
2025-05-16 13:01:50 +0900

    React整理途中5

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import UrlInputForm from "./UrlInputForm";
-import BasicAuthForm from "./BasicAuthForm";
-import HiddenElementsForm from "./HiddenElementsForm";
-import BrowserSelect from "./BrowserSelect";
-import ScreenshotButton from "./ScreenshotButton";
+import UrlInputForm from "./components/UrlInputForm";
+import BasicAuthForm from "./components/BasicAuthForm";
+import HiddenElementsForm from "./components/HiddenElementsForm";
+import BrowserSelect from "./components/BrowserSelect";
+import ScreenshotButton from "./components/ScreenshotButton";
 
 export default function Home() {
     const [url, setUrl] = useState("");
