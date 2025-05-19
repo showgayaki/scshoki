abd224406bee7e645e9f52bf2b6dcae62e9be756
showgayaki <knk.lee.kj@gmail.com>
2025-05-20 01:19:13 +0900

    Tailwind CSS導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./app/App";
-import './styles/main.scss';
+import './styles/globals.css';
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
